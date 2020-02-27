e03e7f51ed4a53e243a9d196e7083cda57a7c9c5
Shehr Yar <shehry@gentrack.com>
2020-02-27 11:45:51 +0000

    file added

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 
-# Create EC2 instance
+provider "aws" {
+  region = "eu-west-1"  # Uses this as a default region to deploy resources
+  version = "~> 2.50"   # Freezing AWS Provider version is a good practice in case next one breaks something
+}
+
+# Create EC2 instance in DEFAULT VPC
 resource "aws_instance" "test-shehr" {
-  ami                    = "ami-021acbdb89706aa89"
+  ami                    = "ami-009bdfd61d16a1793"
   count                  = "1"
   key_name               = "padlock1"
-  vpc_security_group_ids = "${aws_security_group.default.id}"
+  vpc_security_group_ids = [aws_security_group.default.id]
   source_dest_check      = false
   instance_type          = "t3.small"
 
@@ -30,3 +35,4 @@ resource "aws_security_group" "default" {
     protocol    = "tcp"
     cidr_blocks = ["81.108.106.54/32"]
   }
+}
